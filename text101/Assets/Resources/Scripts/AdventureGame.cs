71392b710b558261e4bc23cc6f2fcb5d85ca309f
William Santos <williamdsw@outlook.com>
2020-12-11 11:43:24 -0300

    Uploading text101

---
+++ text101/Assets/Resources/Scripts/AdventureGame.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class AdventureGame : MonoBehaviour
+{
+    // [SerializeField] = Permite deixar a variavel visivel no Inspector
+    [SerializeField] private TextMeshProUGUI textComponent;
+    [SerializeField] private State startingState;
+
+    private State state;
+
+    // Start is called before the first frame update
+    private void Start ()
+    {
+        state = startingState;
+        textComponent.text = state.GetStateStory ();
+    }
+
+    // Update is called once per frame
+    private void Update ()
+    {
+        MenageState ();
+
+        if (Input.GetKeyDown (KeyCode.Q))
+        {
+            Application.Quit ();
+        }
+    }
+
+    //-------------------------------------------------------------------------------------//
+    // HELPER FUNCTIONS
+
+    // Changes actual state and update the text
+    private void MenageState ()
+    {
+        var nextStates = state.GetNextStates ();
+
+        // Eliminates IndexOutOfBounds...
+        for (int index = 0; index < nextStates.Length; index++)
+        {
+            if (Input.GetKeyDown (KeyCode.Alpha1 + index))
+            {
+                state = nextStates[index];
+            }
+        }
+
+        textComponent.text = state.GetStateStory ();
+    }
+}
---
+++ text101/Assets/Resources/Scripts/State.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+// CreateAssetMenu = Permite criar um objeto desse script
+[CreateAssetMenu (menuName = "State")]
+public class State : ScriptableObject
+{
+    // TextArea (min, max) = Permite criar um "textarea" com numero minimo e maximo de linhas
+    [SerializeField] [TextArea (14, 10)] 
+    private string storyText;
+
+    [SerializeField] 
+    private State[] nextStates;
+
+    public string GetStateStory ()
+    {
+        return storyText;
+    }
+
+    public State[] GetNextStates ()
+    {
+        return nextStates;
+    }
+}
