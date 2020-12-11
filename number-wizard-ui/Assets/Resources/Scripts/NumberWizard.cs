fb043ec25db210aaad86cbc91ffcc2dbb177b36f
William Santos <williamdsw@outlook.com>
2020-12-11 11:54:20 -0300

    Adding Number Wizard UI

---
+++ number-wizard-ui/Assets/Resources/Scripts/NumberWizard.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class NumberWizard : MonoBehaviour
+{
+    [SerializeField] private int min;
+    [SerializeField] private int max;
+    [SerializeField] private int guess;
+    [SerializeField] private TextMeshProUGUI guessText;
+
+    private void Start ()
+    {
+        StartGame ();
+    }
+
+    private void StartGame ()
+    {
+        NextGuess ();
+    }
+
+    // Selects a random number between the min and max numbers
+    private void NextGuess ()
+    {
+        guess = Random.Range (min, max + 1);
+        guessText.text = guess.ToString ();
+    }
+
+    // Pass the guess number + 1 to min number
+    public void OnPressHigher ()
+    {
+        min = (guess + 1);
+        NextGuess ();
+    }
+
+    // Pass the guess number - 1 to max number
+    public void OnPressLower ()
+    {
+        max = (guess - 1);
+        NextGuess ();
+    }
+}
---
+++ number-wizard-ui/Assets/Resources/Scripts/SceneLoader.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    // Loads the next scene based on currentSceneIndex + 1
+    public void LoadNextScene ()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene ().buildIndex;
+        SceneManager.LoadScene (currentSceneIndex + 1);
+    }
+
+    // Load the first scene
+    public void LoadStartScene ()
+    {
+        SceneManager.LoadScene (0);
+    }
+
+    // Quit the game
+    public void QuitGame ()
+    {
+        Application.Quit ();
+    }
+}
